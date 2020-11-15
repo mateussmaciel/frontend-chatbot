e8ee7cf42ff8caf2fd09ebbdc111c25e641413df
mateussmaciel <mateus.fmaciel@uni9.edu.br>
2020-11-15 02:37:59 -0300

    att

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import api from '../../services/api';
 
-import {Title, Form, User, Watson} from './style';
+import {Form, User, Watson, Container, Content} from './style';
 
 
 const Dashboard = () => {
@@ -9,6 +9,12 @@ const Dashboard = () => {
   const[message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
 
+  useEffect(() => {scrollWin()}, [chat])
+  function scrollWin() {
+    var elmnt = document.getElementById("A");
+    elmnt.scrollTop = Number.MAX_SAFE_INTEGER
+    
+  }
   async function handleSendMessage(event){
     event.preventDefault();
     try{ 
@@ -18,8 +24,11 @@ const Dashboard = () => {
       const response = await api.post('/', {message: message});
       const responseMessage = response.data;
       const messagesWatson = chat;
-      messagesWatson.push({message: responseMessage.message, who: 'watson'})
+      let parsed = new DOMParser();
+      console.log(parsed.parseFromString(responseMessage, "text/html"))
+      messagesWatson.push({message:  responseMessage, who: 'watson'})
       setChat([...chat, messagesWatson]);      
+      
       setMessage('');
       setInputError('');
     }catch(err){
@@ -27,27 +36,47 @@ const Dashboard = () => {
     }    
   }
   return (
-    <>
-      <Title>
-        Envie uma mensagem para o chatbot watson!
-      </Title>
+    <Container>
+    <Content>
+    <div style={{display:'flex', flexDirection:'column',width:'48%', marginLeft:'20px', maxHeight: '655px'}}>
+      <h1>
+        Consultor Tecnológico Watson Assistent
+      </h1>
+      <h4 style={{lineHeight: 1.5, overflow: 'auto', maxHeight: '550px', paddingRight:'10px'}}>
+      <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quam diam, efficitur sit amet mauris ac, tempus rutrum neque. Nullam rutrum aliquet lobortis. Pellentesque eu turpis accumsan, tempor mi nec, gravida orci. Sed eu tristique sapien, nec bibendum enim. Integer hendrerit mi mauris, in semper libero molestie eu. Fusce vitae ultricies eros. Donec orci eros, tincidunt vitae elit eu, pulvinar consectetur libero. Donec sollicitudin non est quis pulvinar. Quisque eros quam, finibus a blandit non, efficitur id arcu. Nulla nec arcu eget est tempus egestas sit amet malesuada arcu.</p>
+      <p align="justify">Cras ut urna laoreet orci gravida maximus. Phasellus venenatis dapibus lobortis. Fusce efficitur pharetra aliquet. Morbi consequat tortor eros, gravida commodo erat dapibus pretium. Sed lacinia nec purus sed tempus. Donec at porttitor velit, lobortis fermentum velit. Phasellus turpis nisl, auctor in lobortis vitae, rhoncus lacinia sem. Nunc dolor sapien, varius quis malesuada quis, vestibulum ac tellus.</p>
+      <p align="justify">Sed at mi in ex feugiat tristique. Morbi id congue nulla. Praesent eleifend tincidunt purus a laoreet. Vivamus a lorem nisl. Sed mollis feugiat diam ac gravida. Vivamus sit amet dolor odio. Etiam auctor, lorem eget accumsan accumsan, sapien lectus scelerisque risus, nec consequat orci turpis et purus. Donec sollicitudin posuere arcu, quis faucibus sapien bibendum sit amet. Proin dui purus, volutpat vitae consectetur a, finibus non quam. Integer laoreet, nulla eget sodales bibendum, tortor arcu ornare eros, id rhoncus mi mauris in lacus. Ut ultricies erat non tortor ultrices tempor. Aliquam tempor vitae metus lobortis fringilla. Nam sed convallis mi, at faucibus urna. Cras et ullamcorper dui, sed vehicula tellus.</p>
+
+      </h4>
+      </div>
+    <div id="A" style={{display:'flex',flexDirection:'column', maxHeight: '530px', overflow: 'auto'}}>
+        {
+           chat && Array.isArray(chat[chat.length-1]) ? chat.slice(chat.length-1, 1) : ""
+          }
+          {chat.map((item, index) => (
+            item.who === 'user'
+            ? <User key={index} > {item.message} </User> 
+            : Array.isArray(item) ? "" :<Watson key={index}> {item.message} </Watson>
+            
+          ))}
+        </div>
+         
+      <div style={{display:'flex',flexDirection:'column',width:'48%', maxHeight: '650px', overflow: 'auto', alignSelf:'flex-end', position:'absolute', bottom:23}}>
       <Form hasError={!! inputError} onSubmit={handleSendMessage}>
         <input
           value={message}
           onChange={(e ) => setMessage(e.target.value)}
           placeholder="Escreva uma mensagem"         
          />
-        <button type="submit">Enviar</button>
+        <button type="submit" >Enviar</button>
       </Form>
-      {
-       chat && Array.isArray(chat[chat.length-1]) ? chat.slice(chat.length-1, 1) : ""
-      }
-      {chat.map((item, index) => (
-        item.who === 'user'
-        ? <User key={index}> {item.message} </User>
-        : Array.isArray(item) ? "" :<Watson key={index}> {item.message} </Watson>
-      ))}
-    </>
+      </div>
+      
+      
+      
+      
+      </Content>
+    </Container>
   );
 }
 
--- src/pages/Dashboard/style.js
+++ src/pages/Dashboard/style.js
@@ -1,29 +1,37 @@
 import styled, {css} from 'styled-components';
 import {shade} from 'polished';
 
-export const Title = styled.h1`
-  font-size: 48px;
-  color: #3A3A3A;
-  max-width: 450px;
-  line-height: 56px;
-  margin-top:80px;
+export const Container = styled.div`
+  height: 95vh;
+  flex-direction: column;
+  display: flex;
 `;
 
-export const Form = styled.form`
-  margin-top:40px;
-  max-width: 700px;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 95vh;
+  max-width: 1700px;
+`;
 
+
+
+export const Form = styled.form`
+margin-top:20px;
+  max-width: 650px;  
   display:flex;
+  align-self: flex-end;
 
   input{
     flex:1;
-    height: 70px;
+    height: 66px;
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3A3A3A;
     border: 2px solid #ADADAD;
     border-right: 0;
+    width: 550px;
 
     ${(props) => props.hasError && css`
       border-color: #c53030;
@@ -36,6 +44,7 @@ export const Form = styled.form`
 
   button{
     width: 210px;
+    height: 70px;
     background: #04D361;
     border-radius: 0px 5px 5px 0px;
     border: 0;
@@ -52,18 +61,22 @@ export const Form = styled.form`
 
 export const Messages = styled.div`
   margin-top:10px;
-  max-width: 700px;
   flex:1;
-  height: 70px;
+  margin-left: 21px;
+  max-height: 20px;
+  height: 30px;
+  width:600px;
   border-radius: 5px 5px 5px 5px;
   color: #3A3A3A;
   padding: 24px 24px;
 `;
 
 export const User = styled(Messages)`
+  
   background: #04D361;
   color: #000;
   text-align: end;
+  
 `;
 
 export const Watson = styled(Messages)`
